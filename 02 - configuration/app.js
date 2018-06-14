3964ce0537c5bc0ab7baf45ee65e374736368ca4
Dilven <mateusiakkrystian@gmail.com>
2018-06-14 21:46:19 +0200

    init commit

--- 02 - configuration/app.js
+++ 02 - configuration/app.js
@@ -1,5 +1,20 @@
-const express = require('express')
-
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const app = express();
+const routes = require('./routes/index');
+const flash = require('connect-flash');
 
+app.set('views', path.join(__dirname, "views"));
+app.set('view engine', 'pug');
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(cookieParser());
+app.use(flash());
 module.exports = app;
+
+app.use('/', routes);
+
+
